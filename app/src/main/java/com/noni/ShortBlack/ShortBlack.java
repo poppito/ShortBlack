0989272ead435dcca25f56fe7dc6b4d3fc314110
Harsh Overseer <harsh@embry.io>
2016-09-30 21:37:27 +1000

    - added drawable graphics for short black icon - fullscreen activity now!

--- app/src/main/java/com/noni/ShortBlack/ShortBlack.java
+++ app/src/main/java/com/noni/ShortBlack/ShortBlack.java
@@ -1,12 +1,15 @@
 package com.noni.ShortBlack;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -29,6 +32,19 @@ public class ShortBlack extends AppCompatActivity implements View.OnClickListene
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT < 16) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        } else {
+            View decorView = getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+            ActionBar ab = this.getSupportActionBar();
+            if (ab != null) {
+                ab.hide();
+            }
+        }
+
         setContentView(R.layout.activity_short_black);
         submitButton = (Button) findViewById(R.id.submitButton);
         nameText = (EditText) findViewById(R.id.Name);
@@ -65,7 +81,7 @@ public class ShortBlack extends AppCompatActivity implements View.OnClickListene
 
         //Initialise submit button
         submitButton.setOnClickListener(this);
-       // submitButton.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.button_inactive));
+        // submitButton.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.button_inactive));
         submitButton.setBackgroundColor(getResources().getColor(R.color.button_inactive));
         nameText.addTextChangedListener(this);
         GenerateName g = new GenerateName();
@@ -81,9 +97,10 @@ public class ShortBlack extends AppCompatActivity implements View.OnClickListene
                 if (position != 0) {
                     valuesMap.put("milkChoice", parent.getItemAtPosition(position).toString());
                     Log.v(TAG, parent.getItemAtPosition(position).toString());
-                }
-                else {
-                    if (valuesMap.containsKey("milkChoice")) { valuesMap.remove("milkChoice"); }
+                } else {
+                    if (valuesMap.containsKey("milkChoice")) {
+                        valuesMap.remove("milkChoice");
+                    }
                 }
                 break;
             }
@@ -93,9 +110,10 @@ public class ShortBlack extends AppCompatActivity implements View.OnClickListene
                     //pop this into the json object;
                     valuesMap.put("additiveChoice", parent.getItemAtPosition(position).toString());
                     Log.v(TAG, parent.getItemAtPosition(position).toString());
-                }
-                else {
-                    if (valuesMap.containsKey("additiveChoice")) { valuesMap.remove("additiveChoice"); }
+                } else {
+                    if (valuesMap.containsKey("additiveChoice")) {
+                        valuesMap.remove("additiveChoice");
+                    }
                 }
 
                 break;
@@ -106,9 +124,10 @@ public class ShortBlack extends AppCompatActivity implements View.OnClickListene
                     //pop this into the json object;
                     valuesMap.put("coffeeType", parent.getItemAtPosition(position).toString());
                     Log.v(TAG, parent.getItemAtPosition(position).toString());
-                }
-                else {
-                    if (valuesMap.containsKey("coffeeType")) { valuesMap.remove("coffeeType"); }
+                } else {
+                    if (valuesMap.containsKey("coffeeType")) {
+                        valuesMap.remove("coffeeType");
+                    }
                 }
                 break;
             }
@@ -118,9 +137,10 @@ public class ShortBlack extends AppCompatActivity implements View.OnClickListene
                     //pop this into the json object;
                     valuesMap.put("orderSize", parent.getItemAtPosition(position).toString());
                     Log.v(TAG, parent.getItemAtPosition(position).toString());
-                }
-                else {
-                    if (valuesMap.containsKey("orderSize")) { valuesMap.remove("orderSize"); }
+                } else {
+                    if (valuesMap.containsKey("orderSize")) {
+                        valuesMap.remove("orderSize");
+                    }
                 }
                 break;
             }
@@ -128,24 +148,9 @@ public class ShortBlack extends AppCompatActivity implements View.OnClickListene
         enableButton();
     }
 
-    public boolean allValuesValidated()
-    {
-        if(valuesMap.containsKey("milkChoice") && (valuesMap.containsKey("additiveChoice")) && (valuesMap.containsKey("coffeeType") && valuesMap.containsKey("orderSize"))) {
+    public boolean allValuesValidated() {
+        if (valuesMap.containsKey("milkChoice") && (valuesMap.containsKey("additiveChoice")) && (valuesMap.containsKey("coffeeType") && valuesMap.containsKey("orderSize"))) {
             return true;
-        }
-        else if (! valuesMap.containsKey("milkChoice")) {
-            Toast.makeText(getApplicationContext(), "Please select a choice for milk type!", Toast.LENGTH_SHORT).show();
-
-        }
-        else if (! valuesMap.containsKey("additiveChoice")) {
-            Toast.makeText(getApplicationContext(), "Please select an additive choice your order!", Toast.LENGTH_SHORT).show();
-        }
-        else if (! valuesMap.containsKey("coffeeType")) {
-            Toast.makeText(getApplicationContext(), "Please select type of coffee for your order!", Toast.LENGTH_SHORT).show();
-        }
-        else if (! valuesMap.containsKey("orderSize")) {
-            Toast.makeText(getApplicationContext(), "Please select an order size!", Toast.LENGTH_SHORT).show();
-            Log.v(TAG, "no order size selected");
         }
         return false;
     }
@@ -159,7 +164,6 @@ public class ShortBlack extends AppCompatActivity implements View.OnClickListene
 
     @Override
     public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
     }
 
     @Override
@@ -175,14 +179,12 @@ public class ShortBlack extends AppCompatActivity implements View.OnClickListene
         }
     }
 
-    public void enableButton()
-    {
-        if(allValuesValidated()) {
+    public void enableButton() {
+        if (allValuesValidated()) {
 
             //submitButton.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.button_active));
             submitButton.setBackgroundColor(getResources().getColor(R.color.button_active));
-        }
-        else {
+        } else {
             //submitButton.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.button_inactive));
             submitButton.setBackgroundColor(getResources().getColor(R.color.button_inactive));
         }
@@ -199,10 +201,20 @@ public class ShortBlack extends AppCompatActivity implements View.OnClickListene
                     fireUpOrderDetails.putExtra("orderDetails", valuesMap);
                     fireUpOrderDetails.putExtra("nameGenerated", name);
                     startActivity(fireUpOrderDetails);
+                } else if (!valuesMap.containsKey("milkChoice")) {
+                    Toast.makeText(getApplicationContext(), "Please select a choice for milk type!", Toast.LENGTH_SHORT).show();
+
+                } else if (!valuesMap.containsKey("additiveChoice")) {
+                    Toast.makeText(getApplicationContext(), "Please select an additive choice your order!", Toast.LENGTH_SHORT).show();
+                } else if (!valuesMap.containsKey("coffeeType")) {
+                    Toast.makeText(getApplicationContext(), "Please select type of coffee for your order!", Toast.LENGTH_SHORT).show();
+                } else if (!valuesMap.containsKey("orderSize")) {
+                    Toast.makeText(getApplicationContext(), "Please select an order size!", Toast.LENGTH_SHORT).show();
+                    Log.v(TAG, "no order size selected");
                 }
             }
-                break;
-            }
+            break;
         }
-
     }
+
+}
